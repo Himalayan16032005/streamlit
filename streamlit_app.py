34398af167af738c664d0ac1674028ade77bb38d
Saswata Bose <135855951+Himalayan16032005@users.noreply.github.com>
2024-04-21 17:57:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.header('Find the :rainbow[maximum] of :rainbow[three] numbers', divider='rainbow')
-num1 = st.number_input(label = "Enter 1st number", value = None, placeholder = "1st number...")
-num2 = st.number_input(label = "Enter 2nd number", value = None, placeholder = "2nd number...")
-num3 = st.number_input(label = "Enter 3rd number", value = None, placeholder = "3rd number...")
+num1 = st.number_input(label = "Enter :rainbow[1st] number", value = None, placeholder = "1st number...")
+num2 = st.number_input(label = "Enter :rainbow[2nd] number", value = None, placeholder = "2nd number...")
+num3 = st.number_input(label = "Enter :rainbow[3rd] number", value = None, placeholder = "3rd number...")
 if (num1 != None and num2 != None and num3 != None):
     st.write("Max value among the three numbers: ",  str(max(num1, num2, num3)))
