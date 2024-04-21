073629f77f6fcdf02e8d72ed66a397a7cf58a110
Saswata Bose <135855951+Himalayan16032005@users.noreply.github.com>
2024-04-21 17:52:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ num1 = st.number_input(label = "Enter 1st number", value = None, placeholder = "
 num2 = st.number_input(label = "Enter 2nd number", value = None, placeholder = "2nd number...")
 num3 = st.number_input(label = "Enter 3rd number", value = None, placeholder = "3rd number...")
 if (num1 != None and num2 != None and num3 != None):
-    st.write("Max value among the three numbers: ", str(max(num1, num2, num3)))
+    "Max value among the three numbers: ",  str(max(num1, num2, num3))
