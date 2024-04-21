8e2558e0de5afc07cfec6fa4d72609692ad40d14
Saswata Bose <135855951+Himalayan16032005@users.noreply.github.com>
2024-04-21 17:58:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ num1 = st.number_input(label = "Enter :rainbow[1st] number", value = None, place
 num2 = st.number_input(label = "Enter :rainbow[2nd] number", value = None, placeholder = "2nd number...")
 num3 = st.number_input(label = "Enter :rainbow[3rd] number", value = None, placeholder = "3rd number...")
 if (num1 != None and num2 != None and num3 != None):
-    st.write("Max value among the three numbers: ",  str(max(num1, num2, num3)))
+    st.write(":rainbow[Maximum] value among the three numbers: ",  :rainbow[str(max(num1, num2, num3))])
